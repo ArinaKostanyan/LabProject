ee1c28338ab42ede80154ab75db40718a9c12072
Arina Kostanyan <arina@Arinas-MacBook-Pro.local>
2022-08-01 20:55:34 +0400

    Gaussian Blurring Implementation

--- convolution.py
+++ convolution.py
@@ -86,9 +86,9 @@ def combined(A, G_x = None, G_y = None, stride = 1):
             multiplied_v = matrix_section *  G_y
             vert_sum = np.sum(multiplied_v)
             
+            result = np.absolute(horizon_sum) + np.absolute(vert_sum)
             result = math.sqrt(horizon_sum**2 + vert_sum**2)
-            anchor[i][j] = np.clip(result, 0, 255)
+            
+            anchor[i][j] = result
             
     return cv.convertScaleAbs(anchor)
-
-# def gaussian_blurring(a, kernel):
--- opening.py
+++ opening.py
@@ -3,20 +3,21 @@ import cv2 as cv
 import numpy as np
 import os
 import convolution 
+import gaussian
 
-
-path = r'/Users/arina/Desktop/LabProject/images/images.jpeg'
+path = r'/Users/arina/Desktop/LabProject/images/lena.png'
 directory = r'/Users/arina/Desktop/LabProject/images'
 
 
 # Тhis method loads an image from the specified file
 img = cv.imread(path)
 
-# Gaussian bluring
-Gaussian = cv.GaussianBlur(img, (3, 3), 0)
-
 # Converts an image from one color space to another
-gray = cv.cvtColor(Gaussian,cv.COLOR_BGR2GRAY)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# Gaussian blurring
+Gaus = gaussian.blurring(gray)
+
 
 
 # Change the current directory to specified directory 
@@ -27,42 +28,27 @@ anchor1 = convolution.horizontal_convulation(gray)
 anchor2 = convolution.vertical_convulation(gray)
 anchor = convolution.combined(gray)
 
-anchor_weighted = cv.addWeighted(anchor1,0 ,  anchor2, 1, 0)
-filename = 'anchorweighted_gaus.jpeg'
-cv.imshow(filename, anchor_weighted)
-cv.imwrite(filename, anchor_weighted)
 
-scale = 1
-delta = 0
-ddepth = cv.CV_16S
-
-grad_x = cv.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale,
-                  delta=delta, borderType=cv.BORDER_DEFAULT)
-grad_y = cv.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale,
-                  delta=delta, borderType=cv.BORDER_DEFAULT)
-
-abs_grad_x = cv.convertScaleAbs(grad_x)
-abs_grad_y = cv.convertScaleAbs(grad_y)
+# Printing Gaussian blurred image 
+# filename = 'Gaus.jpeg'
+# cv.imshow(filename, Gaus)
+# cv.imwrite(filename, Gaus)
 
 
 # Vertical and horizontal convolution printing
-"""filename = 'convertScaleAbs(anchor1).jpeg_gaus'
+filename = 'convertScaleAbs(anchor1)_gaus.jpeg'
 cv.imshow(filename, anchor1)
-# Saving the image
 cv.imwrite(filename, anchor1)
 
 filename = 'convertScaleAbs(anchor2)_gaus.jpeg'
 cv.imshow(filename, anchor2)
 cv.imwrite(filename, anchor2)
-"""
 
-
-"""filename = 'edgeweighted_gaus.jpeg'
-cv.imshow(filename, cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0))
-cv.imwrite(filename, cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0))
-"""
-
+#Combined edge detected image printing
 filename = 'Edge_detected.jpeg'
 cv.imshow(filename, anchor)
 cv.imwrite(filename, anchor)
+
+
 cv.waitKey(0)
+cv.destroyAllWindows()
