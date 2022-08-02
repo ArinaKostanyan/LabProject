b418dc57c9ce6cca74d314d839d6cfc80f9cefbe
ArinaKostanyan <arina@Arinas-MacBook-Pro.local>
2022-08-02 21:44:05 +0400

    Sobel edge detection method

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-
 import cv2 as cv
-import os
-import convolution 
+
+import convolution
 import gaussian
 
-path = r'/Users/arina/Desktop/LabProject/images/lena.png'
-directory = r'/Users/arina/Desktop/LabProject/images'
-
+path = input('Enter your path or default will be picture of Lena \n')
 
 # Тhis method loads an image from the specified file
 img = cv.imread(path)
+    
+if img == None or img == '':
+    path = 'image/lena.png'
+    img = cv.imread(path)
 
 # Converts an image from one color space to another
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+
 # Gaussian blurring
 Gaus = gaussian.blurring(gray)
-
-
-
-# Change the current directory to specified directory 
-os.chdir(directory)
-
 
 anchor1 = convolution.horizontal_convulation(gray)
 anchor2 = convolution.vertical_convulation(gray)
 anchor = convolution.combined(gray)
-
 
 # Printing Gaussian blurred image 
 # filename = 'Gaus.jpeg'
@@ -43,11 +38,10 @@ filename = 'convertScaleAbs(anchor2)_gaus.jpeg'
 cv.imshow(filename, anchor2)
 cv.imwrite(filename, anchor2)
 
-#Combined edge detected image printing
+# Combined edge detected image printing
 filename = 'Edge_detected.jpeg'
 cv.imshow(filename, anchor)
 cv.imwrite(filename, anchor)
-
 
 cv.waitKey(0)
 cv.destroyAllWindows()
