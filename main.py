235262899285c7c8e28bada9f27fb9dc91d89519
ArinaKostanyan <arina@Arinas-MacBook-Pro.local>
2022-08-03 22:22:31 +0400

    Modified Sobel edge detection method

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-import cv2 as cv
+import image
 
-import convolution
-import gaussian
+hint = "Enter your path or type pass and default will be picture of Lena. \n \t"
 
-path = input('Enter your path or default will be picture of Lena \n')
+count_of_path = 0
+ 
+flag = True
 
-# Тhis method loads an image from the specified file
-img = cv.imread(path)
+sobel_kernel = None
+gaussian_kernel = None
+
+while flag:
+    if count_of_path > 20:
+        print("You've entered path name more than 20 times. Program will be stopped.")
+        break
+    count_of_path+=1
     
-if img == None or img == '':
-    path = 'image/lena.png'
-    img = cv.imread(path)
-
-# Converts an image from one color space to another
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-
-# Gaussian blurring
-Gaus = gaussian.blurring(gray)
-
-anchor1 = convolution.horizontal_convulation(gray)
-anchor2 = convolution.vertical_convulation(gray)
-anchor = convolution.combined(gray)
-
-# Printing Gaussian blurred image 
-# filename = 'Gaus.jpeg'
-# cv.imshow(filename, Gaus)
-# cv.imwrite(filename, Gaus)
-
-
-# Vertical and horizontal convolution printing
-filename = 'convertScaleAbs(anchor1)_gaus.jpeg'
-cv.imshow(filename, anchor1)
-cv.imwrite(filename, anchor1)
-
-filename = 'convertScaleAbs(anchor2)_gaus.jpeg'
-cv.imshow(filename, anchor2)
-cv.imwrite(filename, anchor2)
-
-# Combined edge detected image printing
-filename = 'Edge_detected.jpeg'
-cv.imshow(filename, anchor)
-cv.imwrite(filename, anchor)
-
-cv.waitKey(0)
-cv.destroyAllWindows()
+    path = input(hint)
+    
+    if path == "pass":
+        print("Image of Lena is going to be operated.")
+        path = 'imagefolder/lena.png'
+        
+    if not path.endswith(('.jpeg', '.jpg', '.png')):
+        print("You've entered invalid path. \n")
+    else:
+        flag = False  
+else:
+    image.processing(path, sobel_kernel, gaussian_kernel)
